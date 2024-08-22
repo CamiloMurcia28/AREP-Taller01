6d14e25a56f62c95141c445fccacffd502d6aaf6
CamiloMurcia28 <camilo281913@gmail.com>
2024-08-22 01:19:48 -0500

    Final

--- src/main/resources/webroot/appointments.js
+++ src/main/resources/webroot/appointments.js
@@ -1,35 +1,50 @@
-document.getElementById('citaForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    var doctor = document.getElementById('doctor').value;
-    var fecha = document.getElementById('fecha').value;
-    var tipo = document.getElementById('tipo').value;
-    
-    // Enviar datos al servidor
-    fetch('/app/addMedAppointment', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            doctor: doctor,
-            fecha: fecha,
-            tipo: tipo
+document.getElementById('citaForm').addEventListener('submit', loadPostMsg);
+
+function loadPostMsg(event) {
+    event.preventDefault();
+
+    const doctor = document.getElementById('doctor').value;
+    const fecha = document.getElementById('fecha').value;
+    const tipo = document.getElementById('tipo').value
+    let url = `/app/addMedAppointment?docName=${encodeURIComponent(doctor)}&schDate=${encodeURIComponent(fecha)}&type=${encodeURIComponent(tipo)}`;
+
+    fetch(url, { method: 'POST' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
         })
-    })
-    .then(response => response.json())
-    .then(data => {
-        var resultado = document.getElementById('resultado');
-        resultado.innerHTML = `
-            <h2>Cita Programada:</h2>
-            <p><strong>Doctor:</strong> ${data.doctor}</p>
-            <p><strong>Fecha:</strong> ${data.fecha}</p>
-            <p><strong>Tipo de Cita:</strong> ${data.tipo}</p>
-        `;
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        var resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<p>Error al programar la cita. Por favor, intente de nuevo.</p>';
-    });
-});
+        .then(data => {
+            document.getElementById('message').innerText = `Cita con: ${data.docName} añadido correctamente`;
+            fetchAppointments();
+        })
+        .catch(error => {
+            console.error("Error al añadir cita:", error);
+            document.getElementById('message').innerText = 'Error al añadir cita';
+        }); 
+}
+
+
+function fetchAppointments() {
+    fetch('/app/medAppointment')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log("Data received:", data);
+            let appointmentItems = document.getElementById('appointmentItems');
+            appointmentItems.innerHTML = '';
+            data.medAppointment.forEach(medAppointment => {
+                let li = document.createElement('li');
+                li.textContent = `Cita: ${medAppointment.docName}, Fecha: ${medAppointment.schDate}`;
+                appointmentItems.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar las citaaaaaaaaaaas:', error);
+            document.getElementById('message').innerText = 'Error al cargar las citaas.';
+        });
+}
+
+window.onload = fetchAppointments();
